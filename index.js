97454833d02a0375a9d20ff7d3ba14db0e6809f8
princemaster2 <110698769+princemaster2@users.noreply.github.com>
2023-11-24 19:46:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,132 +1,152 @@
 const config = require('./config.js');
 
-const makeWASocket = require("@whiskeysockets/baileys").default;
-const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@whiskeysockets/baileys");
-const util = require("util");
-const { useMultiFileAuthState, jidDecode, makeInMemoryStore, DisconnectReason, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys");
-const logger = require("@whiskeysockets/baileys/lib/Utils/logger").default;
-const pino = require("pino");
-const gp = ["254114018035"];
-const fs = require("fs");
-const figlet = require("figlet");
-const chalk = require("chalk");
-const os = require("os");
-const speed = require("performance-now");
-const timestampe = speed();
-const dreadedspeed = speed() - timestampe;
+const makeWASocket = require("@whiskeysockets/baileys").default; 
 
-const spinnies = new(require('spinnies'))();
-const { Boom } = require("@hapi/boom");
+ const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@whiskeysockets/baileys"); 
 
-global.store = makeInMemoryStore({
-  logger: pino().child({
-    level: "silent",
-    stream: "store",
-  }),
-});
+ const util = require("util"); 
 
-async function main() {
-  console.log(
-    chalk.red(
-      figlet.textSync("PRINCE_M-XIV", {
-        font: "Standard",
-        horizontalLayout: "default",
-        verticalLayout: "default",
-        whitespaceBreak: false,
-      })
-    )
-  );
+ const { useMultiFileAuthState, jidDecode, makeInMemoryStore, DisconnectReason, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys"); 
+ const logger = require("@whiskeysockets/baileys/lib/Utils/logger").default; 
+ const pino = require("pino"); 
+ const gp = ["254114018035"];  
+ const fs = require("fs"); 
+ const figlet = require("figlet"); 
+ const chalk = require("chalk"); 
+ const os = require("os"); 
+ const speed = require("performance-now"); 
+ const timestampe = speed(); 
+   const dreadedspeed = speed() - timestampe 
 
-  const { state, saveCreds } = await useMultiFileAuthState("session");
+ const spinnies = new(require('spinnies'))(); 
 
-  const mokaya = makeWASocket({
-    logger: pino({
-      level: "silent",
-    }),
-    printQRInTerminal: true,
-    browser: ["Prince Active", "safari", "1.0.0"],
-    auth: state,
-    qrTimeout: 20000000,
-  });
+ const { Boom } = require("@hapi/boom"); 
+ const color = (text, color) => { 
+   return !color ? chalk.green(text) : chalk.keyword(color)(text); 
+ }; 
 
-  mokaya.ev.on("messages.upsert", async (chatUpdate) => {
-    const m = chatUpdate.messages[0];
-    m.chat = m.key.remoteJid;
-    m.fromMe = m.key.fromMe;
-    m.sender = mokaya.decodeJid(
-      m.fromMe && mokaya.user.id
-        ? m.fromMe
-        : m.participant || m.key.participant || m.chat
-    );
 
-    const groupMetadata = m.isGroup
-      ? await mokaya.groupMetadata(m.chat).catch((e) => {})
-      : "";
-    const groupName = m.isGroup ? groupMetadata.subject : "";
 
-    if (!m.message) return;
+ // const { Socket } = Extra; 
+ global.store = makeInMemoryStore({ 
+  logger: pino().child({ 
+     level: 'silent', 
+     stream: 'store'  
+   }) 
+ }); 
 
-    // Handle incoming messages
-    await handleIncomingMessage(m, mokaya);
+ function smsg(m, conn) { 
+   if (!m) return; 
+   let M = proto.WebMessageInfo; 
+   if (m.key) { 
+     m.id = m.key.id; 
+     m.isBaileys = m.id.startsWith("BAE5") && m.id.length === 16; 
+     m.chat = m.key.remoteJid; 
+     m.fromMe = m.key.fromMe; 
+     m.isGroup = m.chat.endsWith("@g.us"); 
+     m.sender = conn.decodeJid((m.fromMe && conn.user.id) || m.participant || m.key.participant || m.chat || ""); 
+     if (m.isGroup) m.participant = conn.decodeJid(m.key.participant) || ""; 
+   } 
+   return m; 
+ } 
+ async function main () { 
+ // const main = async () => { 
 
-    // Add the rest of your logic here...
-  });
+   const { state, saveCreds } = await useMultiFileAuthState('session'); 
+ console.log( 
+     color( 
+       figlet.textSync("PRINCE_M-XIV", { 
+         font: "Standard", 
+         horizontalLayout: "default", 
+         vertivalLayout: "default", 
+         whitespaceBreak: false, 
+       }), 
+       "red" 
+     ) 
+   ); 
 
-  mokaya.decodeJid = (jid) => {
-    if (!jid) return jid;
-    if (/:\d+@/gi.test(jid)) {
-      let decode = mokaya.jidDecode(jid) || {};
-      return (decode.user && decode.server && decode.user + "@" + decode.server) || jid;
-    } else return jid;
-  };
 
-  mokaya.ev.on("connection.update", async (update) => {
-    const { connection, lastDisconnect, qr } = update;
 
-    if (lastDisconnect == "undefined" && qr != "undefined") {
-      qrcode.generate(qr, {
-        small: true,
-      });
-    }
 
-    if (connection === "connecting") {
-      spinnies.add("start", {
-        text: "Connecting Now. . .",
-      });
-    } else if (connection === "open") {
-      spinnies.succeed("start", {
-        text: `Successfully Connected. You have logged in as ${mokaya.user.name}`,
-      });
-    } else if (connection === "close") {
-      if (lastDisconnect.error.output.statusCode == DisconnectReason.loggedOut) {
-        spinnies.fail("start", {
-          text: `Can't connection!`,
-        });
+   const mokaya = makeWASocket({  
+           logger: pino({ 
+          level: 'silent' 
+       }), 
+     printQRInTerminal: true, 
+     browser: ['Prince Active', 'safari', '1.0.0'], 
+     auth: state, 
+qrTimeout: 20000000,
 
-        process.exit(0);
-      } else {
-        main().catch(() => main());
-      }
-    }
-  });
+   }); 
 
-  mokaya.ev.on("creds.update", saveCreds);
-}
 
-// Add your function to handle incoming messages
-async function handleIncomingMessage(message, client) {
-  // Your logic for handling incoming messages
-  // You can use the 'message' and 'client' parameters here
-  // For example:
-  if (message.fromMe) {
-    // Message is from the bot
-    console.log('Message from bot:', message.content.text);
-  } else {
-    // Message is from a user
-    console.log('Message from user:', message.content.text);
-    // Your response logic here...
-    await client.sendMessage(message.chat, 'Hello! This is a response from your WhatsApp bot.');
-  }
-}
 
-main();
+
+
+ mokaya.ev.on('messages.upsert', async chatUpdate => { 
+
+           m = chatUpdate.messages[0] 
+  m.chat = m.key.remoteJid; 
+  m.fromMe = m.key.fromMe; 
+  m.sender = mokaya.decodeJid((m.fromMe && mokaya.user.id) || m.participant || m.key.participant || m.chat); 
+
+
+           const groupMetadata = m.isGroup ? await mokaya.groupMetadata(m.chat).catch((e) => {}) : ""; 
+  const groupName = m.isGroup ? groupMetadata.subject : ""; 
+
+
+  if (!m.message) return 
+  
+
+
+
+  }); 
+
+  mokaya.decodeJid = (jid) => { 
+     if (!jid) return jid; 
+     if (/:\d+@/gi.test(jid)) { 
+       let decode = jidDecode(jid) || {}; 
+       return (decode.user && decode.server && decode.user + "@" + decode.server) || jid; 
+     } else return jid; 
+   }; 
+
+
+
+   mokaya.ev.on('connection.update', async (update) => { 
+       const { 
+          connection, 
+          lastDisconnect, 
+          qr 
+       } = update 
+       if (lastDisconnect == 'undefined' && qr != 'undefined') { 
+          qrcode.generate(qr, { 
+             small: true 
+          }) 
+       } 
+       if (connection === 'connecting') { 
+        spinnies.add('start', { 
+          text: 'Connecting Now. . .' 
+       }) 
+      } else if (connection === 'open') { 
+          spinnies.succeed('start', { 
+             text: `Successfully Connected. You have logged in as ${mokaya.user.name}` 
+          }) 
+       } else if (connection === 'close') { 
+          if (lastDisconnect.error.output.statusCode == DisconnectReason.loggedOut) { 
+             spinnies.fail('start', { 
+                text: `Can't connection!` 
+             }) 
+
+             process.exit(0) 
+          } else { 
+             main().catch(() => main()) 
+          } 
+       } 
+    }) 
+
+
+   mokaya.ev.on('creds.update', saveCreds); 
+
+ }; 
+
+ main();
