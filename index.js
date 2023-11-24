ef02d5b130a3781230db1e2c824fe2a6029ec4ae
Fortunatus Mokaya ☠️ <nyangichafortunatus@gmail.com>
2023-11-24 17:50:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,153 @@
-const { Client, Message, Buttons, BaileysEventEmitter, LocalAuth } = require('@adiwajshing/baileys');
-const botOwner = {
-  name: 'prince',
-  phoneNumber: 'https://wa.me/254768371432'
-};
+const makeWASocket = require("@whiskeysockets/baileys").default; 
+
+ const { BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, proto, generateWAMessageContent, generateWAMessage, prepareWAMessageMedia, areJidsSameUser, getContentType } = require("@whiskeysockets/baileys"); 
+
+ const util = require("util"); 
+
+ const { useMultiFileAuthState, jidDecode, makeInMemoryStore, DisconnectReason, fetchLatestBaileysVersion } = require("@whiskeysockets/baileys"); 
+ const logger = require("@whiskeysockets/baileys/lib/Utils/logger").default; 
+ const pino = require("pino"); 
+ const gp = ["254114018035"];  
+ const fs = require("fs"); 
+ const figlet = require("figlet"); 
+ const chalk = require("chalk"); 
+ const os = require("os"); 
+ const speed = require("performance-now"); 
+ const timestampe = speed(); 
+   const dreadedspeed = speed() - timestampe 
+
+ const spinnies = new(require('spinnies'))(); 
+
+ const { Boom } = require("@hapi/boom"); 
+ const color = (text, color) => { 
+   return !color ? chalk.green(text) : chalk.keyword(color)(text); 
+ }; 
 
 
-const client = new Client({
-  auth: new LocalAuth(),
-  puppeteer: true,
-  sessionId: 'YOUR_SESSION_ID' 
-});
 
-client.on('qr', (qr) => {
-  console.log('QR Code:', qr);
-});
+ // const { Socket } = Extra; 
+ global.store = makeInMemoryStore({ 
+  logger: pino().child({ 
+     level: 'silent', 
+     stream: 'store'  
+   }) 
+ }); 
 
-client.on('open', () => {
-  console.log('WhatsApp connection established.');
-});
+ function smsg(m, conn) { 
+   if (!m) return; 
+   let M = proto.WebMessageInfo; 
+   if (m.key) { 
+     m.id = m.key.id; 
+     m.isBaileys = m.id.startsWith("BAE5") && m.id.length === 16; 
+     m.chat = m.key.remoteJid; 
+     m.fromMe = m.key.fromMe; 
+     m.isGroup = m.chat.endsWith("@g.us"); 
+     m.sender = conn.decodeJid((m.fromMe && conn.user.id) || m.participant || m.key.participant || m.chat || ""); 
+     if (m.isGroup) m.participant = conn.decodeJid(m.key.participant) || ""; 
+   } 
+   return m; 
+ } 
+ async function main () { 
+ // const main = async () => { 
 
-client.on('message', async (message) => {
+   const { state, saveCreds } = await useMultiFileAuthState('session'); 
+ console.log( 
+     color( 
+       figlet.textSync("DREADED  BOT", { 
+         font: "Standard", 
+         horizontalLayout: "default", 
+         vertivalLayout: "default", 
+         whitespaceBreak: false, 
+       }), 
+       "red" 
+     ) 
+   ); 
 
-  const messageText = message.content ? message.content.text : '';
-  const senderId = message.key.remoteJid;
 
-  if (senderId === botOwner.phoneNumber) {
-    console.log('Message from bot owner:', messageText);
-  } else {
-  
-    if (messageText.startsWith('.')) {
-      
-      const command = messageText.substring(1).toLowerCase();
 
-      if (command === 'owner') {
-    
-        const ownerContact = Message.fromContactCard({
-          displayName: botOwner.name,
-          vcard: `BEGIN:VCARD\nVERSION:3.0\nFN:${botOwner.name}\nTEL:${botOwner.phoneNumber}\nEND:VCARD`
-        });
 
-        await client.sendMessage(senderId, ownerContact);
-      } else {
-    
-      }
-    } else {
-   
-    }
-  }
-});
+   const sock = makeWASocket({  
+           logger: pino({ 
+          level: 'silent' 
+       }), 
+     printQRInTerminal: true, 
+     browser: ['Dreaded Active', 'safari', '1.0.0'], 
+     auth: state, 
+qrTimeout: 20000000,
+
+   }); 
+
+
+
+
+
+ sock.ev.on('messages.upsert', async chatUpdate => { 
+
+           m = chatUpdate.messages[0] 
+  m.chat = m.key.remoteJid; 
+  m.fromMe = m.key.fromMe; 
+  m.sender = sock.decodeJid((m.fromMe && sock.user.id) || m.participant || m.key.participant || m.chat); 
+
+
+           const groupMetadata = m.isGroup ? await sock.groupMetadata(m.chat).catch((e) => {}) : ""; 
+  const groupName = m.isGroup ? groupMetadata.subject : ""; 
+
+
+  if (!m.message) return 
+  if (m.chat.endsWith('broadcast')) { 
+          sock.readMessages([m.key]); 
+
+          } 
+
+
+
+  }); 
+
+  sock.decodeJid = (jid) => { 
+     if (!jid) return jid; 
+     if (/:\d+@/gi.test(jid)) { 
+       let decode = jidDecode(jid) || {}; 
+       return (decode.user && decode.server && decode.user + "@" + decode.server) || jid; 
+     } else return jid; 
+   }; 
+
+
+
+   sock.ev.on('connection.update', async (update) => { 
+       const { 
+          connection, 
+          lastDisconnect, 
+          qr 
+       } = update 
+       if (lastDisconnect == 'undefined' && qr != 'undefined') { 
+          qrcode.generate(qr, { 
+             small: true 
+          }) 
+       } 
+       if (connection === 'connecting') { 
+        spinnies.add('start', { 
+          text: 'Connecting Now. . .' 
+       }) 
+      } else if (connection === 'open') { 
+          spinnies.succeed('start', { 
+             text: `Successfully Connected. You have logged in as ${sock.user.name}` 
+          }) 
+       } else if (connection === 'close') { 
+          if (lastDisconnect.error.output.statusCode == DisconnectReason.loggedOut) { 
+             spinnies.fail('start', { 
+                text: `Can't connection!` 
+             }) 
+
+             process.exit(0) 
+          } else { 
+             main().catch(() => main()) 
+          } 
+       } 
+    }) 
+
+
+   sock.ev.on('creds.update', saveCreds); 
+
+ }; 
+
+ main();
